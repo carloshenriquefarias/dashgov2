07d05a6e4719fbaf64e52e9e6db3713b36c03168
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2022-09-16 22:21:26 -0400

    Configuração de input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,16 +21,16 @@ export default function SignIn() {
           w="100%" 
           maxWidth={360}
           bg="gray.800"
-          // SE QUISER SABER EM PX multiplique por 4, se for em rem divide por 4
+          // SE QUISER SABER EM PX multiplique por 4, se for em rem divide por 4 ou mais
           p="8"
           borderRadius={8}
           flexDir="column"
           onSubmit={handleSubmit(handleSignIn)}
         >
           <Stack spacing="4">  
-            <Input name='email' type='email' label='E-mail'ref={register}/> 
+            <Input name='email' type='email' label='E-mail'/> 
             {/* //Para passar ref na apicação é preciso mudar a constante no input Form */}
-            <Input name='password' type='password' label='Senha'ref={register}/>           
+            <Input name='password' type='password' label='Senha'/>           
           </Stack>
         <Button type='submit' mt="6" colorScheme="pink">Entrar</Button>
         </Flex>
