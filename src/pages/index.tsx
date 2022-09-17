b918c53a8e344a02aa96d67d8674a69653232b99
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2022-09-17 12:46:59 -0400

    bug no git?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,24 +34,9 @@ export default function SignIn() {
           </Stack>
         <Button type='submit' mt="6" colorScheme="pink">Entrar</Button>
         </Flex>
-        <Flex 
-          as="form" 
-          w="100%" 
-          maxWidth={360}
-          bg="gray.800"
-          // SE QUISER SABER EM PX multiplique por 4, se for em rem divide por 4 ou mais
-          p="8"
-          borderRadius={8}
-          flexDir="column"
-          onSubmit={handleSubmit(handleSignIn)}
-        >
-          <Stack spacing="4">  
-            <Input name='email' type='email' label='E-mail'/> 
-            {/* //Para passar ref na apicação é preciso mudar a constante no input Form */}
-            <Input name='password' type='password' label='Senha'/>           
-          </Stack>
-        <Button type='submit' mt="6" colorScheme="pink">Entrar</Button>
-        </Flex>
+        <div>
+          bug noi git?
+        </div>
     </Flex>
   )
 }
