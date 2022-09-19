b230d946fefe781baa0efee4ceb8d1b1bc4764e0
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2022-09-19 09:48:25 -0400

    Validação do formulario

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import {Flex, Button, Stack } from '@chakra-ui/react'
 import {Input} from '../components/Form/Input'
 import {useForm } from "react-hook-form"
+import * as yup from 'yup'
+import {yupResolver } from "@hookform/resolvers/yup"
 
 export default function SignIn() {
 
